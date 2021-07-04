2d9eee8c7b1eb82137f7b9d3e905e3f511e560e1
Hans Vilu <47756053+Echoround@users.noreply.github.com>
2021-07-04 18:17:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,29 @@ scaled_data = min_max.fit_transform(data['Close'].values.reshape(-1,1))
 
 prediction_range = 60
 
+x_train = []
+y_train = []
+
+for i in range(prediction_range, len(scaled_data)):
+    x_train.append(scaled_data[i-prediction_range:i, 0])
+    y_train.append(scaled_data[x,0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# the model (LSTM layers follower by Droput layers until a final Dense layer that gives us the stock prediction)
+
+model = Sequential()
+
+model.add(LSTM(units=45, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=45, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=45))
+model.add(Dense(units=1)) #prediction of next closing value
+
+
+
+
+
+
